858262907c28c32c837e010e8c7048b5a210b5c8
William Elizondo <0618506375@my.browardschools.com>
2024-01-31 13:29:23 -0500

    1/31/2024 1:29PM

---
+++ ChapterThreeLogic/Elizondo_ChapThreeExerciseFour.py
@@ -0,0 +1,6 @@
+# Automobile Costs
+# Design a modular program that asks the user to enter 
+#the monthly costs for the following exspenses incurred from operating his or her automobile: loan payment,
+# insurance, gas, oil, tires and maintenance. The program should then display the total monthly cost of these 
+#exspenses, and the total annual cost of these exspenses.
+
