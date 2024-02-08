305a6c5582d643ed82b91efa7226ba578a34c189
William Elizondo <0618506375@my.browardschools.com>
2024-02-08 08:49:18 -0500

    2/8/2024 8:50am

---
+++ ChapterFourLogic/ChapFourLogicExerciseFive.py
@@ -0,0 +1,33 @@
+# Design a program that prompts the user to enter the name of the two primary colors to mix. If the user enters anything other than "red", "blue", or "yellow" the program should display an error message. Otherwise the program should display the name of the secondary color that results.
+# First color to mix
+print("Enter your first color to mix!")
+firstColor = input()
+
+# second color to mix
+print("Enter your second color to mix!")
+secondColor = input()
+
+# making sure the inputs are valid before mixing colors
+if firstColor == "red" or firstColor == "blue" or firstColor == "yellow" and secondColor == "red" or secondColor == "blue" or secondColor == "yellow":
+    
+    # mixing colors
+    # mixing purple
+    if firstColor == "red" and secondColor == "blue" or firstColor == "blue" and secondColor == "red":
+        mixColor = "purple"
+    else:
+        
+        # mixing orange
+        if firstColor == "red" and secondColor == "yellow" or secondColor == "red" and firstColor == "yellow":
+            mixColor = "orange"
+        else:
+            
+            # mixing green
+            if firstColor == "blue" and secondColor == "yellow" or firstColor == "yellow" and secondColor == "blue":
+                mixColor = "green"
+            else:
+                print("error")
+    
+    # end result
+    print(firstColor + " combined with " + secondColor + " equal " + mixColor)
+else:
+    print("error")
---
+++ ChapterFourLogic/ChapFourLogicExerciseNine.py
@@ -0,0 +1,32 @@
+#Shipping Charges
+# The Fast Freight Shipping Company charges the following rates:
+# Weight of Package Rate per Pound
+# 2 pounds or less $1.10
+# Over 2 pounds but not more than 6 pounds $2.20
+# Over 6 pounds but not more than 10 pounds $3.70
+# Over 10 pounds $3.80
+# Design a program that asks the user to enter the weight of a package and then displays the shipping
+# charges
+#
+
+#user inputs package weight
+print("Enter the weight of the package")
+packWeight = float(input())
+#if packWeight less than 2 pounds than price is 1.10
+if packWeight <= 2:
+    price = 1.10
+else:
+    #if packWeight is less than 6 but more than 2 than price is 2.20
+    if packWeight < 6:
+        if packWeight > 2:
+            price = 2.20
+    else:
+        #if packWeight is less than 10 but more than 6 than price is 3.70
+        if packWeight < 10:
+            if packWeight > 6:
+                price = 3.70
+        else:
+            #if packWeight is more than 10 than price is 3.80
+            if packWeight > 10:
+                price = 3.80
+print(price)
