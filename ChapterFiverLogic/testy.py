e29eaae7f64fdb86684bcd77882fbd4267172d08
William Elizondo <will.elizond00@gmail.com>
2024-02-16 15:53:51 -0500

    eijseiohjf

--- ChapterFiverLogic/testy.py
+++ ChapterFiverLogic/testy.py
@@ -1,6 +1,20 @@
-years = 0
+years = 2 # years
+MTHS = 12 # months
 
-years = int(input("Please enter the number of years "))
+totalMthsAllYears = 0 # accumlation for all the years 
+totalMthsPerYear = 0 # acculation for each year
 
-for year in range(years):
-    for month in range(12):
+inputMths = 0 # input variable
+
+for y in range(years) : # outer (3 times)
+
+    #totalMthsPerYear = 0 # accumulation for each year
+    for m in range(MTHS): # Inner (12 Times)
+        inputMths = int(input("Please enter the Month: "))
+        totalMthsPerYear = totalMthsPerYear + inputMths
+        print("Total Months: ", totalMthsPerYear)
+
+    totalMthsAllYears = totalMthsAllYears + totalMthsPerYear
+    print("Total mths per year", totalMthsAllYears)
+
+print("Total of all months for all years ", totalMthsAllYears)
