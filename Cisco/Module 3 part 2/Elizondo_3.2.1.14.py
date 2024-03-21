f4f7cd1e110a35a8cf2b324e59d0a502735bd439
William Elizondo <will.elizond00@gmail.com>
2024-03-21 16:46:29 -0400

    yeaaa

---
+++ Cisco/Module 3 part 2/Elizondo_3.2.1.14.py
@@ -0,0 +1,43 @@
+﻿# William Elizondo
+# 3.2.1.14 Counting Stack
+#
+class Stack:
+    def __init__(self):
+        # Initialize a private stack list when a Stack instance is created
+        self.__stk = []
+
+    def push(self, val):
+        # Add an element to the top of the stack
+        self.__stk.append(val)
+
+    def pop(self):
+        # Remove and return the top element to the stack
+        val = self.__stk[-1]
+        del self.__stk[-1]
+        return val
+
+
+class CountingStack(Stack):
+    def __init__(self):
+        # Call the constructor of the Stack class to initialize the stack
+        Stack.__init__(self)
+        # Initialize a private counter to track the number of pop operations
+        self.__counter = 0
+
+    def get_counter(self):
+        # Return the current value of the counter
+        return self.__counter
+
+    def pop(self):
+        # Increment the counter on each pop operation
+        self.__counter += 1
+        # Call the pop method of the base Stack class and return it's value
+        return Stack.pop(self)
+
+
+stk = CountingStack() # Create an instance of CountingStack
+for i in range(100):
+    # Push and immediately pop an element 100 times
+    stk.push(i)
+    stk.pop()
+print(stk.get_counter()) # Print the number of pop operations performed.
---
+++ Cisco/Module 3 part 2/Elizondo_3.2.1.15.py
@@ -0,0 +1,43 @@
+﻿# William Elizondo
+# 3.2.1.15 LAB: Queue aka FIFO
+#
+# Define a custom exeception for queue-related errors, inheriting from IndexError
+class QueueError(IndexError):
+    pass
+
+#Define a Queue class to implement queue functionality 
+class Queue:
+    def __init__(self):
+        # Initialize the queue with the empty list
+        self.queue = []
+
+    def put(self, elem):
+        # Add an element to the beginning of the queue
+        self.queue.insert(0, elem)
+
+    def get(self):
+        # Remove and return the last element of the queue if it's not empty
+        if len(self.queue) > 0:
+            elem = self.queue[-1]
+            del self.queue[-1]
+            return elem
+        else:
+            # If the queue is empty raise a custom QueueError
+            raise QueueError
+
+# Create an instance of the Queue class
+que = Queue()
+
+# Add elements to the queue using the put method
+que.put(1)
+que.put("dog")
+que.put(False)
+
+# Attempt to get and print elements from the queue
+try:
+    # Try to get and print elements from the queue four times
+    for i in range(4):
+        print(que.get())
+except:
+    # Catch and print a message if a Queue Error is raised
+    print("Queue error")
