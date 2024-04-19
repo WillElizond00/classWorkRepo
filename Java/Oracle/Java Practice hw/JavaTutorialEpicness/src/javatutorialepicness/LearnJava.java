e964e8f7c1c85244aa14c2778b247b541a61051d
William Elizondo <will.elizond00@gmail.com>
2024-04-19 16:31:07 -0400

    yes

---
+++ Java/Oracle/Java Practice hw/JavaTutorialEpicness/src/javatutorialepicness/LearnJava.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javatutorialepicness;
+/**
+ *
+ * @author deck
+ */
+public class LearnJava {
+    public static void main(String[] args){
+        
+        addExclamationPoint("hot dogs");
+   
+    }
+    
+    public static void addExclamationPoint(String s){
+        System.out.println(s + "!");
+    }
+}
